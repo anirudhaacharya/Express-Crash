4659e9c17d850845d1a2d83846756e2ed1ff530f
Anirudha <54995711+anirudhaacharya@users.noreply.github.com>
2024-07-01 14:51:21 +0530

    First Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
+import logger from './middleware/logger.js';
+const port = process.env.PORT || 8080;
+
+
+const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+// Logger Middleware
+app.use(logger);
+
+
+// setup static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Routes
+app.use('/api/posts', posts);
+
+
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
